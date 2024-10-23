7b873df0cfff76b3617acc477ef43ae9ec8cc7e6
yousofkortam <yousofkortam@outlook.com>
2024-10-23 23:21:49 +0300

    chore: handle store service implementation

--- src/app/services/store.service.ts
+++ src/app/services/store.service.ts
@@ -1,9 +1,47 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../environment/environment.prod';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Store } from '../models/store';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StoreService {
 
-  constructor() { }
+  baseUrl: string = `${environment.apiUrl}/store-api`;
+
+  constructor(private http: HttpClient) { }
+
+  getStores(page: number, size: number): Observable<Store[]> {
+    return this.http.get<Store[]>(`${this.baseUrl}/stores?page=${page}&size=${size}`);
+  }
+
+  getStoreById(id: number): Observable<Store> {
+    return this.http.get<Store>(`${this.baseUrl}/stores/${id}`);
+  }
+
+  createStore(store: Store): Observable<Store> {
+    return this.http.post<Store>(`${this.baseUrl}/stores`, store);
+  }
+
+  updateStore(id: number, store: Store): Observable<Store> {
+    return this.http.put<Store>(`${this.baseUrl}/stores/${id}`, store);
+  }
+
+  deleteStore(id: number): Observable<Store> {
+    return this.http.delete<Store>(`${this.baseUrl}/stores/${id}`);
+  }
+
+  searchProductsByStoreId(storeId: number, name: string, category: string, page: number, size: number): Observable<Store> {
+    let params = new HttpParams()
+      .append('name', name)
+      .append('category', category);
+      
+    return this.http.get<Store>(
+      `${this.baseUrl}/stores/${storeId}/products?${category}&page=${page}&size=${size}`,
+      { params: params }
+    );
+  }
+
 }
