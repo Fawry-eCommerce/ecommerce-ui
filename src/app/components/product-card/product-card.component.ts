5d373fe4214f9a93b8218cf71dcfbfdd19d8a499
yousofkortam <yousofkortam@outlook.com>
2024-10-26 22:12:17 +0300

    chrore: handle product card router link

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -2,14 +2,15 @@ import { Component, Input } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { Product } from '../../models/product/Product';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [CardModule, ButtonModule],
+  imports: [CardModule, ButtonModule, RouterLink],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.css'
 })
 export class ProductCardComponent {
-  @Input() product!: Product; // TODO: Define the type of the product
+  @Input() product!: Product;
 }
--- src/app/guards/admin.guard.ts
+++ src/app/guards/admin.guard.ts
@@ -1,6 +1,6 @@
 import { CanActivateFn } from '@angular/router';
 
 export const adminGuard: CanActivateFn = (route, state) => {
-  // TODO: Implement your admin guard logic here
+  // This is a mock implementation of an admin guard
   return true;
 };
