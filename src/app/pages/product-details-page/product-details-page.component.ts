e47474bd42eed1f7c184d368645c99e71e26b664
yousofkortam <yousofkortam@outlook.com>
2024-10-28 15:43:46 +0300

    feat: add product details page

--- src/app/pages/product-details-page/product-details-page.component.ts
+++ src/app/pages/product-details-page/product-details-page.component.ts
@@ -1,22 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import { MatCard, MatCardContent, MatCardHeader, MatCardTitle } from '@angular/material/card';
 import { ActivatedRoute } from '@angular/router';
+import { Product } from '../../models/product/Product';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-product-details-page',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCard,
+    MatCardHeader,
+    MatCardTitle,
+    MatCardContent
+  ],
   templateUrl: './product-details-page.component.html',
   styleUrl: './product-details-page.component.css'
 })
 export class ProductDetailsPageComponent implements OnInit {
 
   productId: number = 0;
+  product!: Product;
 
-  constructor(private activatedRoute: ActivatedRoute) { }
+  constructor(
+    private productService: ProductService,
+    private activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
     this.activatedRoute.params.subscribe(params => {
       this.productId = params['id'];
+      this.getProductById(this.productId);
+    });
+  }
+
+  addToCart(product: Product) {
+
+  }
+
+  getProductById(id: number) {
+    this.productService.getProduct(id).subscribe({
+      next: (res) => {
+        console.log(res);
+        
+        this.product = res;
+      },
+      error: error => {
+        console.error(error);
+      }
     });
   }
 
